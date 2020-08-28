765ec44ec91820fdee18172b1a9b25d81fb7a0b4
rrferrao28 <69963967+rrferrao28@users.noreply.github.com>
2020-08-28 06:45:33 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,3 +1,9 @@
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, AdaBoostClassifier
+from sklearn.feature_selection import RFECV
+from sklearn.metrics import classification_report
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
@@ -14,3 +20,24 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+
+!pip install imbalanced-learn==0.4.3 
+
+class Balanceamento(BaseEstimator, TransformerMixin):
+        def __init__(self, X, y):
+            self.X = X
+            self.y = y
+
+
+        def fit(self, X, y):
+            return self
+
+        def transform(self, X, y):
+            from imblearn.over_sampling import SMOTE
+            smote = SMOTE(sampling_strategy='all')
+            # Primeiro realizamos a cópia do dataframe 'X' de entrada
+            self.X, self.y = smote.fit_resample(X,y)
+            # Retornamos um novo dataframe sem as colunas indesejadas
+            return self.X, self.y
