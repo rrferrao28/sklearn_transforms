32bb629581c44ce511d0d410cdf7e1a953396c16
rrferrao28 <69963967+rrferrao28@users.noreply.github.com>
2020-08-31 03:38:33 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,7 +1,6 @@
   
 from sklearn.base import BaseEstimator, TransformerMixin
 import imblearn
-from imblearn.over_sampling import SMOTE
 import pandas as pd
 
 
@@ -20,21 +19,3 @@ class DropColumns(BaseEstimator, TransformerMixin):
         return data.drop(labels=self.columns, axis='columns')
 
 
-
-
-class Balanceamento(BaseEstimator, TransformerMixin):
-        def __init__(self, X, y):
-            self.X = X
-            self.y = y
-
-
-        def fit(self, X, y):
-            return self
-
-        def transform(self, X, y):
-            from imblearn.over_sampling import SMOTE
-            smote = SMOTE(sampling_strategy='all')
-            # Primeiro realizamos a cópia do dataframe 'X' de entrada
-            self.X, self.y = smote.fit_resample(X,y)
-            # Retornamos um novo dataframe sem as colunas indesejadas
-            return self.X, self.y
